3d5bd6d627480689b8c630880649dddf10ade030
victor-kipyegon-koech <victorkipyegon778@gmail.com>
2025-10-19 16:14:35 +0300

    add other fuctionalitie

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import Header from "@/components/header"
 import { ClerkProvider } from "@clerk/nextjs"
+import { dark } from "@clerk/themes"
 
 const inter = Inter({subsets:["latin"]})
  
@@ -16,7 +17,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-     <ClerkProvider>
+     <ClerkProvider appearance={{
+      baseTheme:dark,
+     }
+
+     }>
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className}`}>
         <ThemeProvider
